0132166fe67cb2da9b2aa63f264ad442755db5d9
hodanov <1031hoda@gmail.com>
2023-12-04 10:31:39 +0900

    Rename a file and a class.

--- setup_files/__main__.py
+++ setup_files/__main__.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from setup import stub
-from txt2img import StableDiffusion
+from stable_diffusion_1_5 import Txt2Img
 
 
 @stub.function(gpu="A10G")
 def main():
-    StableDiffusion
+    Txt2Img
 
 
 if __name__ == "__main__":
